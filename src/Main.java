13809f563be284debd5a4f1deb4929b88640c097
tusderechos <hidrosoluciones61@gmail.com>
2025-10-16 16:39:31 -0600

    commit inicial para probar conexion Bash con Github

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("nfdjibgjaebgashlbgsagbaskgbsjkgbsa");
+    }
+    
+}
