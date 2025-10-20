cf35461e6d7232d8026b17cf526e40d228d44397
tusderechos <hidrosoluciones61@gmail.com>
2025-10-20 17:03:45 -0600

    Commit sobre la creacion de MiCuenta y Reportes para futura imlementacion de estos dos mismos

---
+++ src/MiCuenta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MiCuenta extends JFrame {
+    
+}
+
---
+++ src/Reportes.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Hp
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Reportes extends JFrame {
+    
+}
+
