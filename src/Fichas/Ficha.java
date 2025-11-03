716ee75bcbd001daf2c31aca8b24c416e8537496
tusderechos <hidrosoluciones61@gmail.com>
2025-11-02 20:13:01 -0600

    solo me tomo como 10 siglos va pero ahora si hagamos el juego todo otropedo

--- src/Fichas/Ficha.java
+++ src/Fichas/Ficha.java
@@ -4,10 +4,98 @@
  */
 package Fichas;
 
+
+
 /**
  *
  * @author Hp
  */
-public class Ficha {
+
+import Tablero.Tablero;
+import java.util.ArrayList;
+
+public abstract class Ficha {
     
+    private String Nombre;
+    private Bando Color;
+    private Posicion Pos;
+    private int Ataque;
+    private int Vidas;
+    private int Escudo;
+
+    public Ficha(String Nombre, Bando Color, int Ataque, int Vidas, int Escudo) {
+        this.Nombre = Nombre;
+        this.Color = Color;
+        this.Ataque = Ataque;
+        this.Vidas = Vidas;
+        this.Escudo = Escudo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+    
+    public Bando getColor() {
+        return Color;
+    }
+    
+    public Posicion getPos() {
+        return Pos;
+    }
+    
+    public void setPos(Posicion pos) {
+        this.Pos = pos;
+    }
+    
+    public int getAtaque() {
+        return Ataque;
+    }
+    
+    public int getVidas() {
+        return Vidas;
+    }
+    
+    public int getEscudo() {
+        return Escudo;
+    }
+    
+    public boolean Viva() {
+        return Vidas > 0;
+    }
+
+    public void RecibirDano(int puntos) {
+         if (Escudo > 0) {
+             int puntosrestantes = puntos - Escudo; //Esto existe por si digamos que algo magicamente hace 6 puntos de ataque, quitaria primeramente los 5 puntos de escudo, y lo que queda despues de eso es lo que se le quita a la vida
+             Escudo -= puntos;
+
+             if (Escudo < 0) {
+                 Escudo = 0;
+             }
+             if (puntosrestantes > 0) {
+                 Vidas -= puntosrestantes;
+             }
+         } else {
+             Vidas -= puntos;
+         }
+
+         if (Vidas < 0) {
+             Vidas = 0;
+         }
+    }
+   
+    public void Mover(Posicion nuevapos) {
+        this.Pos = nuevapos;
+    }
+    
+    public void Eliminar() {
+        this.Vidas = 0;
+    }
+    
+    public abstract ArrayList<Posicion> MovimientosBasicos(Tablero tablero);
+    
+    public abstract ArrayList<Posicion> MovimientosEspeciales(Tablero tablero);
+    
+    public abstract ArrayList<Posicion> AtaquesNormales(Tablero tablero);
+    
+    public abstract ArrayList<Posicion> AtaquesEspeciales(Tablero tablero);
 }
---
+++ src/Fichas/Posicion.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Fichas;
+
+/**
+ *
+ * @author Hp
+ */
+public class Posicion {
+    
+    private int Fila;
+    private int Col;
+    
+    public Posicion(int Fila, int Col) {
+        this.Fila = Fila;
+        this.Col = Col;
+    }
+    
+    @Override
+    public boolean equals(Object objeto) {
+        if (this == objeto) {
+            return true;
+        }
+        if (objeto == null || getClass() != objeto.getClass()) {
+            return false;
+        }
+        
+        Posicion pos = (Posicion) objeto;
+        
+        return Fila == pos.Fila && Col == pos.Col;
+    }
+
+    @Override
+    public int hashCode() {
+        int resultado = 17;
+        resultado = 31 * resultado + Fila;
+        resultado = 31 * resultado + Col;
+        return resultado;
+    }
+    
+    @Override
+    public String toString() {
+        return "(" + Fila + ", " + Col + ")";
+    }
+}
